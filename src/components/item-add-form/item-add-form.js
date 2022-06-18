cd213a0fe3d2575447bb07a2b8d14e3bbfa16721
Bertalucci <bertamalfitano@yandex.ru>
2022-06-18 19:22:48 +0300

    Add form submitting

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -2,14 +2,38 @@ import React, {Component} from 'react';
 import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
+
+    state = {
+        label: ''
+    };
+
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        });
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();//предотвращает перезагрузку страницы сразу после сабмита формы
+        this.props.onItemAdded(this.state.label);
+        this.setState({
+            label: ''
+        });
+    }
+
     render() {
         return (
-            <div className="item-add-form">
-                <button className="btn btn-outline-secondary"
-                        onClick={() => this.props.onItemAdded('Hello App')}>
-                    Add Item
-                </button>
-            </div>
+            //d-flex - выстраивает элементы друг за другом
+            <form   className="item-add-form d-flex"
+                    onSubmit={this.onSubmit}>
+                <input  type="text"
+                        className="form-control"
+                        onChange={this.onLabelChange}
+                        placeholder="Enter a task"
+                        value={this.state.label}
+                />
+                <button className="btn btn-outline-secondary">Add Item</button>
+            </form>
         );
     }
 }
